08073718285c30ea6e5cf362808fd3955a118569
Jonathan Perry <jpper@umich.edu>
2020-11-19 10:34:34 +0900

    Upgraded version of android build gradle and added dismiss of loading if the ready value of database init didn't succeed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,42 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Component } from "@angular/core";
+import { LoadingController, Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { DatabaseService } from "./services/database.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private databaseService: DatabaseService,
+    private loadingCtrl: LoadingController
   ) {
     this.initializeApp();
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+  async initializeApp() {
+    this.platform.ready().then(async () => {
+      const loading = await this.loadingCtrl.create();
+      await loading.present();
+      this.databaseService.init();
+      this.databaseService.dbReady.subscribe((isReady) => {
+        console.log("Ready is: ", isReady);
+        if (isReady) {
+          loading.dismiss();
+          this.statusBar.styleDefault();
+          this.splashScreen.hide();
+        } else {
+          // Else, dismiss the loading to prevent app from breaking
+
+          loading.dismiss();
+        }
+      });
     });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DatabaseService } from "../services/database.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  products = [];
+  export = null;
+  newProduct = "My cool product";
+  constructor(private databaseService: DatabaseService) {
+    this.loadProducts();
+  }
 
-  constructor() {}
+  loadProducts() {
+    this.databaseService.getProductList().subscribe((res) => {
+      this.products = res.values;
+      console.log("my products:", res);
+    });
+  }
 
+  // Mode is "partial" or "full"
+  async createExport(mode) {
+    const dataExport = await this.databaseService.getDatabaseExport(mode);
+    this.export = dataExport.export;
+  }
+
+  async addProduct() {
+    await this.databaseService.addDummyProduct(this.newProduct);
+    this.newProduct = "";
+    this.loadProducts();
+  }
+
+  async deleteProduct(product) {
+    await this.databaseService.deleteProduct(product.id);
+    this.products = this.products.filter((p) => p != product);
+  }
+
+  deleteDatabase() {
+    this.databaseService.deleteDatabase();
+  }
 }
